7a022975b227d0b9189392d04123b92e08712fe4
tkjelsrud <tkjelsrud@gmail.com>
2018-04-16 13:39:39 +0000

    Create chords.js

---
+++ chords.js
@@ -0,0 +1,5 @@
+var chordList = {"ionian": ["maj", "min7", "min7", "maj7", "dom7", "min7", "dim"]};
+var chordNotes = {"maj": [1, 3, 5], "maj7": [1, 3, 5, 7], "min": [1, "b3", 5], "min7": [1, "b3", 5, "b7"]};
+var allNotesInScale = 12;
+var notes = ["C", "C# Db", "D", "D# Eb", "E", "F", "F# Gb", "G", "G# Ab", "A", "A# Bb", "B"];
+var positions {"1": 0, "b2": 1, "2": 2, "b3": 3, "3": 4, "X"}
