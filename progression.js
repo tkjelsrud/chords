b626159970a944e53b1d5405a9e5f5c03ba30601
tkjelsrud <tkjelsrud@gmail.com>
2023-08-18 12:11:22 +0200

    Create progression.js

---
+++ progression.js
@@ -0,0 +1,23 @@
+const commonProgRaw = {
+  'test': 'I V vi IV',
+  'test2': 'I V ♭VII IV',
+};
+
+// This quickly becomes complex, need to match progression labels to potential key, but key could be any to match
+// Maybe a easier way is to measure the distance between the chords?
+
+function progressionToDistance(progList) {
+  let distList = new Array();
+
+  return distList;
+}
+
+function chordsToProgressions(chordList) {
+  let progression = new Array();
+  
+  // How to balance the complete list (may not fit)
+  // with recent, like the last two chords (progression change)
+  // return array has to be the same length so that it can fit in the same visualization (?)
+
+  return progressions;
+}
