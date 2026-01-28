16de64b1d6b38bf8321efa85bf9343fc1b73389a
tkjelsrud <tkjelsrud@gmail.com>
2026-01-28 10:51:47 +0100

    Switch to vexchords CDN from unpkg for better GitHub Pages compatibility

--- old/lib.js
+++
@@ -1,8 +0,0 @@
-function compareNumbers(arr1, arr2) {
-    eqArr = new Array();
-    for(i = 0; i < Math.min(arr1.length, arr2.length); i++) {
-           if(arr2.indexOf(arr1[i]) >= 0)
-               eqArr.push(arr1[i]);
-    }
-    return eqArr;
-}
